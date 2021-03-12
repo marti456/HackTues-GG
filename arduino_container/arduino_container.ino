e93ed80830c7ef4649262ef3e377904408fe4dc5
marti456 <martidami5@gmail.com>
2021-03-12 13:03:11 +0200

    Simpler Arduino code

--- arduino_container/arduino_container.ino
+++ arduino_container/arduino_container.ino
@@ -23,87 +23,21 @@ int led_state = 0;
  * Functions
  */
 
-// Callback: receiving any WebSocket message
-void onWebSocketEvent(uint8_t client_num,
-                      WStype_t type,
-                      uint8_t * payload,
-                      size_t length) {
-
-  // Figure out the type of WebSocket event
-  switch(type) {
-
-    // Client has disconnected
-    case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", client_num);
-      break;
-
-    // New client has connected
-    case WStype_CONNECTED:
-      {
-        IPAddress ip = webSocket.remoteIP(client_num);
-        Serial.printf("[%u] Connection from ", client_num);
-        Serial.println(ip.toString());
-      }
-      break;
-
-    // Handle text messages from client
-    case WStype_TEXT:
-
-      // Print out raw message
-      Serial.printf("[%u] Received text: %s\n", client_num, payload);
-
-      // Toggle LED
-      if ( strcmp((char *)payload, "activate") == 0 ) {
-        Serial.printf("in If");
-        int i = 0;
-        while(i<20){
-          Serial.printf("in While");
-          digitalWrite(led_pin, HIGH);
-          delay(500);
-          digitalWrite(led_pin, LOW);
-          delay(500);
-          i++;
-        }
-      // Report the state of the LED
-      } else {
-        Serial.println("[%u] Message not recognized");
-      }
-      break;
-
-    // For everything else: do nothing
-    case WStype_BIN:
-    case WStype_ERROR:
-    case WStype_FRAGMENT_TEXT_START:
-    case WStype_FRAGMENT_BIN_START:
-    case WStype_FRAGMENT:
-    case WStype_FRAGMENT_FIN:
-    default:
-      break;
-  }
-}
 
 // Callback: send homepage
-/*void onIndexRequest(AsyncWebServerRequest *request) {
+void onIndexRequest(AsyncWebServerRequest *request) {
   IPAddress remote_ip = request->client()->remoteIP();
   Serial.println("[" + remote_ip.toString() +
                   "] HTTP GET request of " + request->url());
   request->send(SPIFFS, "/index.html", "text/html");
-}*/
+}
 
 // Callback: send style sheet
-/*void onCSSRequest(AsyncWebServerRequest *request) {
+void onCSSRequest(AsyncWebServerRequest *request) {
   IPAddress remote_ip = request->client()->remoteIP();
   Serial.println("[" + remote_ip.toString() +
                   "] HTTP GET request of " + request->url());
   request->send(SPIFFS, "/style.css", "text/css");
-}
-*/
-// Callback: send 404 if requested file does not exist
-void onPageNotFound(AsyncWebServerRequest *request) {
-  IPAddress remote_ip = request->client()->remoteIP();
-  Serial.println("[" + remote_ip.toString() +
-                  "] HTTP GET request of " + request->url());
-  request->send(404, "text/plain", "Not found");
 }
 
 /***********************************************************
